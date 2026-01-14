64ccdf955087a70a12fe6ecaed048aa3069e24b9
MuhammadUzair9481 <mhuammaduzair575@gmail.com>
2026-01-14 12:09:27 +0500

    Create RowWiseSumOfMatrix.cs

---
+++ Multi-dimensional Arrays/RowWiseSumOfMatrix.cs
@@ -0,0 +1,27 @@
+
+using System;
+
+class Rowwiseaddition
+{
+    static void Main()
+    {
+        int[,] matrix =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        
+        for (int i = 0; i < 3; i++)
+        {
+            int rowSum = 0;                  
+
+            for (int j = 0; j < 3; j++)
+            {
+                rowSum = rowSum + matrix[i, j];
+            }
+
+            Console.WriteLine($"Row {i} sum = {rowSum}");
+        }
+    }
+}
